6cbd77834d38672af0ca72553b7b8342220bb39d
amitabhdas96 <amitabh.sweden@gmail.com>
2024-04-08 18:24:12 +1200

    ok

---
+++ Index.js
@@ -0,0 +1,19 @@
+// Question 1
+//function makeCounter(startFrom,incrementBy) {
+    //     let currentCount = startFrom;
+    //     return function() {
+    //     currentCount = currentCount + incrementBy;
+    //     console.log(currentCount)
+    //     return currentCount;
+    //     };
+    //     }
+    
+    //     let counter1 = makeCounter(5,2);
+    //     let counter2 = makeCounter(50,5);
+    
+    //     counter1(); // 1
+    //     counter1(); // 2
+    
+    //     counter2();
+    //     counter2();
+    //     counter2();
